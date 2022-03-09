14cc3765ffcafd5694bb21979b4b825eeecc9c02
arhoads <aaron@eastwatch.com>
2022-03-08 21:59:24 -0600

    MAINT: added .gitignore

--- src/MoodIe/App.jsx
+++ src/MoodIe/App.jsx
@@ -7,10 +7,17 @@ import { GameStates } from './GameState';
 import { $log, css } from '../helpers/dom-helpers';
 
 export default function App() {
+	const [count, setCount] = React.useState(0);
+
 	const game		= useGameState();
 	const gameState	= game.state.gameState;
 
 	function onKeyPress(key) {
+		if (key == 'Enter')
+			setCount(c => c+1);
+		else
+			setCount(0);
+
 		game.onKeyPress(key);
 	}
 
@@ -18,6 +25,9 @@ export default function App() {
 		<div className="appRoot">
 			<div className="content">
 				<Toolbar />
+				{count >= 3 &&
+					<div>{game.state.secretWord}</div>
+				}
 				<GameBoard game={game} />
 				<Keyboard disabled={gameState == GameStates.Scoring || gameState == GameStates.Finished} lettersUsed={game.state.lettersUsed} onKeyPress={onKeyPress} />
 			</div>
