2cbf11dc837f7f177bd0252a05c098fc514c18c4
arhoads <aaron@eastwatch.com>
2022-03-10 13:07:51 -0600

    MAINT: remove debug cheat

--- src/MoodIe/App.jsx
+++ src/MoodIe/App.jsx
@@ -7,27 +7,13 @@ import { GameStates } from './GameState';
 import { $log, css } from '../helpers/dom-helpers';
 
 export default function App() {
-	const [count, setCount] = React.useState(0);
-
 	const game		= useGameState();
 	const gameState	= game.state.gameState;
-
-	function onKeyPress(key) {
-		if (key == 'Enter')
-			setCount(c => c+1);
-		else
-			setCount(0);
-
-		game.onKeyPress(key);
-	}
 
 	return (
 		<div className="appRoot">
 			<div className="content">
 				<Toolbar />
-				{count >= 3 &&
-					<div>{game.state.secretWord}</div>
-				}
 				<GameBoard game={game} />
 				<Keyboard disabled={gameState == GameStates.Scoring || gameState == GameStates.Finished} lettersUsed={game.state.lettersUsed} onKeyPress={onKeyPress} />
 			</div>
